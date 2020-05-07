8f6fc3dc6baa60e5b9a18e4441697fe827ac5911
David Juarez <djuar05@gmail.com>
2020-05-07 16:59:51 -0400

    separated classes one for repeat of game and other to run game

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
         AsciiChars.printNumbers();
         AsciiChars.printLowerCase();
         AsciiChars.printUpperCase();
+        WinningNumbers.lotteryWinner();
         
     }
 }
--- WinningNumbers.java
+++ WinningNumbers.java
@@ -1,9 +1,46 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 public class WinningNumbers {
 
-   public static void lotteryWinner() {
-
+   public static void lotteryWinner(){
        
-   }
-    
+       String playAgain = " ";
+       Scanner scanner = new Scanner(System.in);
+       
+       do{
+        //    WinningNumbers.lotteryWinner();
+           System.out.println("Would you like to play again? Enter Y to play again or any other key to quit: ");
+           playAgain = scanner.nextLine();
+
+       }
+          while(playAgain.equalsIgnoreCase("Y"));
+           System.out.println("Thanks for playing!");
+
+           scanner.close();
+    }
 }
+       
+    // }
+    //    public static void lotteryGuess(){
+
+    //    }
+       
+    //    System.out.println("Welcome, May I have your Name: ");
+       
+    //    while(true){
+    //        String userName = scanner.next();
+        
+    //        System.out.printf("Hello %s would you like to continue to the interactive portion: yes or no? ", userName);
+        
+    //        String userAnswer = scanner.next();
+
+    // if("n".equals(userAnswer) || ("no".equals(userAnswer))){
+    //     System.out.println("Please come back later to finished survey");
+    //     break;
+    // } else {
+        
+    // }
+    
+    // scanner.close();   
+// }
+//    }
+    
